17d9ab41ac4ee67aeb9f8ff1f2e0ff870fb7618f
Weronika <weronikategos9@gmail.com>
2026-09-18 18:02:12 +0200

    Konfiguracja Gradle: moduły projektu (app, shared)

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TableFlow"
+include(":app")
+include(":shared")
